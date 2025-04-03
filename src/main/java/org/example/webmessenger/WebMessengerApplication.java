bf3922eaa0a2c053c7a623471b8b3d51b4a66dcb
Shaaldy <shaldeeman@gmail.com>
2025-04-03 20:19:37 +0500

    chore: инициализация проекта с конфигурацией и окружением

---
+++ src/main/java/org/example/webmessenger/WebMessengerApplication.java
@@ -0,0 +1,13 @@
+package org.example.webmessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMessengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebMessengerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/webmessenger/WebMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.webmessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMessengerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
