fabc7fd437b018ed5e2cff24f773047706ae41c5
Shaaldy <shaldeeman@gmail.com>
2025-04-05 02:30:03 +0500

    initial commit for task2

--- src/main/java/org/example/webmessenger/java/model/User.java
+++ src/main/java/org/example/webmessenger/java/model/User.java
@@ -3,13 +3,19 @@ package org.example.webmessenger.java.model;
 import lombok.*;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class User {
-    private String id;
-    private String login;
     private String email;
     private String name;
+    private String avatarUrl;
+    private String id;
+
+    public User(String email, String name, String picture) {
+        this.email = email;
+        this.name = name;
+        this.avatarUrl = picture;
+        this.id = HashUtils.hashUserFields(email, name, picture);
+    }
 }
